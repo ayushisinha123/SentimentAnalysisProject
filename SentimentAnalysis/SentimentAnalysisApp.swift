34b30040ff38c65b8908067300cf65c884f0fae8
Ayushi Raj <ayushiraj2313@gmail.com>
2024-12-22 18:18:08 +0530

    Initial Commit

---
+++ SentimentAnalysis/SentimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentimentAnalysisApp.swift
+//  SentimentAnalysis
+//
+//  Created by Sushant on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SentimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
