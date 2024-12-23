ba70e8c7296d8cc89cba2a7c80a09a09441cdf87
Ayushi Raj <ayushiraj2313@gmail.com>
2024-12-23 17:29:16 +0530

    Sentiment Analysis Project

--- SentimentAnalysis/ContentView.swift
+++ SentimentAnalysis/ContentView.swift
@@ -4,18 +4,41 @@
 //
 //  Created by Sushant on 22/12/24.
 //
-
 import SwiftUI
+import NaturalLanguage
 
 struct ContentView: View {
+    
+    @State private var inputText: String = ""
+     private var score: String {
+         sentimentAnalyzer(analysingText: inputText)
+    }
+    
+    private func sentimentAnalyzer(analysingText: String) -> String {
+        
+        
+        return sentimentScore(for: analysingText)
+    }
+    
+    let tagger = NLTagger(tagSchemes: [.sentimentScore])
+    
+    private func sentimentScore(for word: String) -> String {
+        tagger.string = word
+        let (sentimentScore, _) = tagger.tag(at: word.startIndex, unit: .paragraph, scheme: .sentimentScore)
+        return sentimentScore?.rawValue ?? ""
+        
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Text("Sentiment Analysis")
+            
+            TextField("Enter Text", text: $inputText)
+            
+            Text(score)
         }
-        .padding()
+        .font(.system(size: 20))
+        .multilineTextAlignment(.center)
     }
 }
 
